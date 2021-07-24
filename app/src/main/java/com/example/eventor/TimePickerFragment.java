d83bbaa23c7f458f9018017811848778cd483622
Zheng Andrew <andrewzheng04@gmail.com>
2021-07-24 14:31:19 -0700

    did date and time

---
+++ app/src/main/java/com/example/eventor/TimePickerFragment.java
@@ -0,0 +1,32 @@
+package com.example.eventor;
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.widget.TimePicker;
+
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Calendar;
+
+public class TimePickerFragment extends DialogFragment
+        implements TimePickerDialog.OnTimeSetListener {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        // Create a new instance of TimePickerDialog and return it.
+        return new TimePickerDialog(getActivity(), this, hour, minute,
+                DateFormat.is24HourFormat(getActivity()));
+    }
+
+    @Override
+    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+        EventsActivity activity = (EventsActivity) getActivity();
+        activity.processTimePickerResult(hourOfDay, minute);
+    }
+}
