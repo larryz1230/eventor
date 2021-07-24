2616afcbbdf748fd2a446a30d2f212514b655833
larryz1230 <larryzhi1230@gmail.com>
2021-07-24 11:42:26 -0700

    login done

--- app/src/main/java/com/example/eventor/LoginActivity.java
+++ app/src/main/java/com/example/eventor/LoginActivity.java
@@ -4,17 +4,43 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
-    Button toEvents, toReg;
+    Button toEvents, toReg, login;
+    public static String ngrokID = "https://e16c423a5a63.ngrok.io";
+    private static String URL_LOGIN = ngrokID + "/eventor/login.php";
+    EditText memail, pass;
+    public static User user;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        memail = findViewById(R.id.email);
+        pass = findViewById(R.id.password);
 
         toEvents = findViewById(R.id.toevents);
         toEvents.setOnClickListener(new View.OnClickListener() {
@@ -31,5 +57,87 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
             }
         });
+
+        login = findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Login(memail.getText().toString().trim(), pass.getText().toString().trim());
+            }
+        });
+
+    }
+
+    private void Login (final String email, final String password){
+
+//            loading.setVisibility(View.VISIBLE);
+//            mLogin.setVisibility(View.GONE);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_LOGIN,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            String success = jsonObject.getString("success");
+                            JSONArray jsonArray = jsonObject.getJSONArray("login");
+
+                            if (success.equals("1")) {
+                                for (int i = 0; i < jsonArray.length(); i++) {
+                                    JSONObject object = jsonArray.getJSONObject(i);
+
+                                    String fname = object.getString("fname").trim();
+                                    String lname = object.getString("lname").trim();
+                                    String email = object.getString("email").trim();
+//                                        String age = object.getString("age").trim();
+                                    int ID = object.getInt("id");
+
+                                    user = new User(ID, fname, lname, email);
+
+                                    Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+
+                                    startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
+
+//                                        loading.setVisibility(View.GONE);
+                                }
+                            } else{
+//                                    loading.setVisibility(View.GONE);
+//                                    login.setVisibility(View.VISIBLE);
+                                memail.setText("");
+                                pass.setText("");
+                                Toast.makeText(LoginActivity.this, "Username or Password incorrect \n Please Try Again", Toast.LENGTH_SHORT).show();
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+//                                loading.setVisibility(View.GONE);
+//                                mLogin.setVisibility(View.VISIBLE);
+                            memail.setText("");
+                            pass.setText("");
+                            Toast.makeText(LoginActivity.this, "Username or Password incorrect \n Please Try Again", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+//                            loading.setVisibility(View.GONE);
+//                            mLogin.setVisibility(View.VISIBLE);
+                        Toast.makeText(LoginActivity.this, "Error" + error.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("email", email);
+                params.put("password", password);
+
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 }
--- app/src/main/java/com/example/eventor/RegisterActivity.java
+++ app/src/main/java/com/example/eventor/RegisterActivity.java
@@ -1,15 +1,35 @@
 package com.example.eventor;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    Button toLog;
+    private static String URL_REGIST = LoginActivity.ngrokID+"/eventor/register.php";
+    Button toLog, Register;
+    private EditText fname, lname, email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +37,87 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         toLog = findViewById(R.id.tolog);
+        Register = findViewById(R.id.register);
         toLog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getApplicationContext(), LoginActivity.class));
             }
         });
+
+        fname = findViewById(R.id.fname);
+        lname = findViewById(R.id.lname);
+        password = findViewById(R.id.password);
+        email = findViewById(R.id.email);
+
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Regist();
+            }
+        });
+    }
+
+    private void Regist(){
+//        mRegister.setVisibility(View.GONE);
+
+        final String fname = this.fname.getText().toString().trim();
+        final String lname = this.lname.getText().toString().trim();
+        final String email = this.email.getText().toString().trim();
+        final String password = this.password.getText().toString().trim();
+
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_REGIST,
+                new com.android.volley.Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try{
+                            JSONObject jsonObject = new JSONObject(response);
+                            String success = jsonObject.getString("success");
+
+
+
+                            if (success.equals("1")){
+                                Toast.makeText(RegisterActivity.this, "Register Success", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+
+                            }
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Log.e("anyText",response);
+                            System.out.println(e.toString());
+                            Toast.makeText(RegisterActivity.this, "Register Error" + e.toString(), Toast.LENGTH_SHORT).show();
+//                            mRegister.setVisibility(View.VISIBLE);
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        System.out.println(error.toString());
+                        Toast.makeText(RegisterActivity.this, "Register Error" + error.toString(), Toast.LENGTH_SHORT).show();
+//                        .setVisibility(View.VISIBLE);
+                    }
+                })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map <String, String> params = new HashMap<>();
+                params.put ("fname", fname);
+                params.put ("lname", lname);
+                params.put("email", email);
+                params.put("password", password);
+
+                System.out.println(fname + " " + lname);
+                return params;
+
+
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 }
